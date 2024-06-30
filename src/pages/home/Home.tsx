a7045ca44fb5fdd3ad43a86d467e63211b42155a
ShawenHarker <harkershawen@gmail.com>
2024-06-30 18:16:00 +0200

    height styling fixed on the home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,14 +1,21 @@
 import { Box } from '@mui/material';
-import theme from '../../utils'
+import theme from '../../utils';
 import NavBar from "../../components/navBar";
 
 export default function Home() {
+    const home = {
+        backgroundColor: theme.palette.background.paper,
+        padding: '1rem 6rem',
+        height: 'calc(100vh - 32px)',
+        overflowY: 'auto',
+        scrollbarWidth: 'none',
+        '&::-webkit-scrollbar': {
+            display: 'none',
+        },
+    };
+
     return (
-        <Box sx={{
-            backgroundColor: theme.palette.background.paper,
-            minHeight: '100vh',
-            padding: '1rem 6rem',
-        }}>
+        <Box sx={home}>
             <NavBar />
         </Box>
     )
