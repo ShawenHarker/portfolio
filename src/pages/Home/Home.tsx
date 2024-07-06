0ea6f6feba3463b3dc038820772ff300a8f98174
ShawenHarker <harkershawen@gmail.com>
2024-07-06 19:03:26 +0200

    loaded all images

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,6 +6,9 @@ export default function Home() {
         title: {
             color: theme.palette.text.primary,
         },
+        subtitle: {
+            color: theme.palette.text.secondary,
+        },
         hire: {
 
         }
@@ -16,16 +19,13 @@ export default function Home() {
             <Typography variant='h1' sx={styles.title}>
                 Hi, I'm Shawen Harker
             </Typography>
-            <Typography variant='h2' color='#00abf0'>A Digital Craftsman (Full-stack developer)</Typography>
+            <Typography variant='h2' style={styles.subtitle}>A Digital Craftsman (Full-stack developer)</Typography>
             <Typography variant='subtitle2' color='primary'>
                 Building my version of the digital world, while specializing in crafting custom software solutions for clients. With a strong foundation in both front-end and back-end technologies, I thrive on turning ideas into functional and elegant applications. I approach every project with agility, constantly learning and evolving to meet the ever-changing demands of the tech industry.
             </Typography>
             <div>
                 <Box sx={styles.hire}>
                     hire me
-                </Box>
-                <Box>
-                    let's talk
                 </Box>
             </div>
         </>
